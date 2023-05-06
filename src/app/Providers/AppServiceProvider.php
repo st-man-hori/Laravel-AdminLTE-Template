2844da4d72d176d4e74da1076b409d1d95164ff6
st-man-hori <77187521+st-man-hori@users.noreply.github.com>
2023-05-07 08:05:22 +0900

    [追加]サービスクラス・インターフェース追加

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Services\Sample\AdminSampleService;
+use App\Services\Sample\AdminSampleServiceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(AdminSampleServiceInterface::class, AdminSampleService::class);
     }
 
     /**
