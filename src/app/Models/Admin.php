50cad8f18db7740f2413c1ebf86cbd0ca7797c3f
st-man-hori <77187521+st-man-hori@users.noreply.github.com>
2023-05-07 07:45:40 +0900

    [追加]AdminModel

--- src/app/Models/Admin.php
+++ src/app/Models/Admin.php
@@ -3,11 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+    use HasApiTokens, HasFactory, Notifiable;
 
     protected $guard = 'admin';
 
@@ -20,5 +20,9 @@ class Admin extends Model
     protected $hidden = [
         'password',
         'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
     ];
 }
